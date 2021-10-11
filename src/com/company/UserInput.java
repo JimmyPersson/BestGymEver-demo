3455a93b9df24fc92c367c8ca7111eef73ba5477
JimmyPersson <jimmy.persson@yh.nackademin.se>
2021-10-11 19:45:58 +0200

    Start of project. Update with new methods.

--- src/com/company/UserInput.java
+++ src/com/company/UserInput.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class UserInput {
@@ -24,19 +26,17 @@ public class UserInput {
 
                     }
                 }
-    private void CheckInputWithFile(){
+
+                private ArrayList GenerateArray(){
+
+        ArrayList memberList = new ArrayList<String>();
 
         try {
             BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\jimmy\\IdeaProjects\\BestGymEver\\src\\com\\company\\customers.txt"));
             String line;
             while ((line = br.readLine()) != null){
-                if (line.contains(input)){
-                    JOptionPane.showMessageDialog(null, "Välkommen!");}
-
-                    else
-                        JOptionPane.showMessageDialog(null, "Get fokt!");
-
-                }
+                memberList.add(line);
+                                  }
 
 
         } catch (FileNotFoundException e) {
@@ -44,6 +44,14 @@ public class UserInput {
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Något blev fel.");
         }
+        return memberList;
+    }
+
+
+    private void CompareList(){
+
+
+
     }
 }
 
