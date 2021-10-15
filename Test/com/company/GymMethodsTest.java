0a7dffa0e2b013c9983d226674151b4bf5d41adc
JimmyPersson <jimmy.persson@yh.nackademin.se>
2021-10-15 14:36:15 +0200

    Last update.

--- Test/com/company/GymMethodsTest.java
+++ Test/com/company/GymMethodsTest.java
@@ -2,6 +2,10 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,6 +38,24 @@ class GymMethodsTest {
     }
 
     @Test
-    void updateFile() {
+    void createFile () {
+        assertFalse(Files.exists(Path.of("C:\\Users\\jimmy\\IdeaProjects\\BestGymEver\\src\\checkin\\Elmer Ekorrsson.txt")));
+        GymMethods test = new GymMethods();
+        List<Person> test2 = test.generateArray();
+        test.createFile(test2.get(4));
+        assertTrue(Files.exists(Path.of("C:\\Users\\jimmy\\IdeaProjects\\BestGymEver\\src\\checkin\\Elmer Ekorrsson.txt")));
+    }
+
+    @Test
+    void updateFile() throws IOException {
+        Files.writeString(Path.of("C:\\Users\\jimmy\\IdeaProjects\\BestGymEver\\src\\checkin\\Test Testsson.txt"), "Incheckningslista.\n" +
+                "Namn: Test Testsson, 8512060099\n" +
+                "2021-10-14");
+        GymMethods test = new GymMethods();
+        List<Person> test2 = test.generateArray();
+        LocalDate time = LocalDate.now();
+        assertFalse(Files.readString(Path.of("C:\\Users\\jimmy\\IdeaProjects\\BestGymEver\\src\\checkin\\Test Testsson.txt")).contains(time.toString()));
+        test.updateFile(new Person("Test Testsson", "8512060099", "2021-02-14"));
+        assertTrue(Files.readString(Path.of("C:\\Users\\jimmy\\IdeaProjects\\BestGymEver\\src\\checkin\\Test Testsson.txt")).contains(time.toString()));
     }
 }
